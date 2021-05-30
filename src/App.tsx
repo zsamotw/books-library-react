af292cd26b3079437928cce6efdab57b7c6eb807
zsamotw <donkemot@interia.pl>
2021-05-30 18:40:22 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import RootComponent from './components/RootComponent';
+import Store from './components/Store';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Store>
+        <RootComponent />
+      </Store>
+    </Router>
   );
 }
 
