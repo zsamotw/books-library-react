14820de74e9cb91c9267abcaf463de927d24e6bc
zsamotw <donkemot@interia.pl>
2021-06-05 09:39:27 +0200

    Use useCallback for memoize dispatch

--- src/components/Store/Store.tsx
+++ src/components/Store/Store.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useReducer } from 'react';
+import React, { ReactNode, useCallback, useReducer } from 'react';
 import { StoreContext } from './store.context';
 import { appReducers } from './reducers';
 import { initialState } from './initial-state';
@@ -10,8 +10,15 @@ type StoreProps = {
 function Store({ children }: StoreProps) {
   const [state, dispatch] = useReducer(appReducers, initialState);
 
+  const memoizedDispatch = useCallback(
+    (action: any) => {
+      dispatch(action);
+    },
+    [dispatch],
+  );
+
   return (
-    <StoreContext.Provider value={{ state, dispatch }}>
+    <StoreContext.Provider value={{ state, dispatch: memoizedDispatch }}>
       <div>{children}</div>
     </StoreContext.Provider>
   );
